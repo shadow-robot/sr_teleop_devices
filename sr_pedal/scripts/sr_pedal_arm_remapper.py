c5ced5e342c923f8fe40bb63da48bf569f39bdf5
carebare47 <tom@shadowrobot.com>
2020-03-18 12:39:18 +0000

    arm pedal remapper

---
+++ sr_pedal/scripts/sr_pedal_arm_remapper.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import rospy
+from std_msgs.msg import Bool
+from sr_pedal.msg import Status
+    
+class SrPedalArmRemapper():
+    def __init__(self):
+        self.subscriber = rospy.Subscriber("sr_pedal/status", Status, self.pedal_sub)
+        self.publisher = rospy.Publisher("sr_arm/release", Bool, queue_size=1)
+        self.rate = rospy.Rate(5)
+        while not rospy.is_shutdown():
+            try:
+                self.rate.sleep()
+            except rospy.ROSInterruptException:
+                rospy.loginfo("Shutting down %s", rospy.get_name())
+
+    def pedal_sub(self, data):
+        if data.connected and data.left_pressed and data.right_pressed:
+            self.publisher.publish(True)
+
+if __name__ == '__main__':
+    rospy.init_node('sr_pedal_arm_remapper', anonymous=True)
+    sr_pedal_arm_remapper = SrPedalArmRemapper()
