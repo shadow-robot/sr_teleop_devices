4395e274d13f706c84020293705e394d7e3ad339
maxzieba <max@shadowrobot.com>
2021-05-07 13:01:57 +0000

    Another fix

--- sr_remappers/src/calibration_parser.cpp
+++ sr_remappers/src/calibration_parser.cpp
@@ -32,10 +32,10 @@
 #include <string>
 #include <vector>
 
-std::string CalibrationParser::default_path[] = "/etc/robot/mappings/default_mapping";
 
 CalibrationParser::CalibrationParser()
 {
+    std::string CalibrationParser::default_path[] = "/etc/robot/mappings/default_mapping";
     ROS_WARN("No calibration path was specified, using default path");
     init(default_path);
 }
