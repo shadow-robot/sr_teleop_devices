c94dd1e5f8ae84e4d3c8637c8a54a3887850c2bd
carebare47 <tom@shadowrobot.com>
2021-07-26 15:14:38 +0000

    fix installer

--- sr_leap_motion/scripts/install_leap.sh
+++ sr_leap_motion/scripts/install_leap.sh
@@ -10,5 +10,6 @@ cd leap
 git clone https://github.com/BlackLight/leap-sdk-python3 && cd leap-sdk-python3 && sed -i 's/^SWIG=swig/SWIG=swig3.0/g' build.sh && make && sudo make install
 cd /opt/leap
 git clone http://github.com/shadow-robot/sr_teleop_devices && cd sr_teleop_devices/sr_leap_motion/scripts && sed -i 's/install/install -y/g' install_sdk.sh && ./install_sdk.sh
+cd /opt/leap && rm -rf sr_teleop_devices
 source /home/user/projects/shadow_robot/base_deps/devel/setup.bash && roscd leap_motion && rm -rf LeapSDK && sudo ln -s /opt/leap/leap-sdk-python3/leap/LeapSDK $(rospack find leap_motion)/LeapSDK
-echo "export PYTHONPATH=\$PYTHONPATH:/home/user/leap-sdk-python3" >> ~/.bashrc
+echo "export PYTHONPATH=\$PYTHONPATH:/opt/leap/leap-sdk-python3" >> ~/.bashrc
