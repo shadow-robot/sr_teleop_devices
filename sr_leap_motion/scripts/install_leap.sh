d2158cc679937050c0e792c7999e9c068bcdc33c
carebare47 <tom@shadowrobot.com>
2021-07-27 10:00:02 +0000

    trying to remove ing

--- sr_leap_motion/scripts/install_leap.sh
+++ sr_leap_motion/scripts/install_leap.sh
@@ -11,5 +11,6 @@ git clone https://github.com/BlackLight/leap-sdk-python3 && cd leap-sdk-python3 
 cd /opt/leap
 git clone http://github.com/shadow-robot/sr_teleop_devices && cd sr_teleop_devices/sr_leap_motion/scripts && sed -i 's/install/install -y/g' install_sdk.sh && ./install_sdk.sh
 cd /opt/leap && rm -rf sr_teleop_devices
-source /home/user/projects/shadow_robot/base_deps/devel/setup.bash && roscd leap_motion && rm -rf LeapSDK && sudo ln -s /opt/leap/leap-sdk-python3/leap/LeapSDK $(rospack find leap_motion)/LeapSDK
+# source /home/user/projects/shadow_robot/base_deps/devel/setup.bash && roscd leap_motion && rm -rf LeapSDK && sudo ln -s /opt/leap/leap-sdk-python3/leap/LeapSDK $(rospack find leap_motion)/LeapSDK
+temp_file=$(mktemp) && for x in $(printf '%s' "$ROS_PACKAGE_PATH" | sed -r 's/:/\n/g'); do echo $x; done > $temp_file && for x in $(awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' $temp_file); do if [[ $(basename $x) == "src" ]]; then echo $x; source $x/../devel/setup.bash; else echo $x; source $x/../setup.bash; fi; done && roscd leap_motion && rm -rf LeapSDK && sudo ln -s /opt/leap/leap-sdk-python3/leap/LeapSDK $(rospack find leap_motion)/LeapSDK
 echo "export PYTHONPATH=\$PYTHONPATH:/opt/leap/leap-sdk-python3" >> ~/.bashrc
